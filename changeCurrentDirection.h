586c7c6e8f5b1c1cc70d4a8e607023de048a9df1
vutrungnghia08101999 <46959272+vutrungnghia08101999@users.noreply.github.com>
2019-04-12 13:13:08 +0700

    Add files via upload

--- changeCurrentDirection.h
+++ changeCurrentDirection.h
@@ -3,8 +3,6 @@
 #include"EnvironmentVariable.h"
 #include<ctype.h>	
 
-vector<string> dirFolder(string path);	//List all folder in this path
-
 /*********************************  MAIN FUNCTION  ****************************************/
 /*
 Pre:
@@ -19,41 +17,26 @@ void changeCurrentDirection(string userInput, string userCommand, EnvironmentVar
 	//cd "     "
 	if (userCommand.size() >= 6 && userCommand.find_first_of('\"') == 3 && userCommand.find_last_of('\"') == userCommand.size() - 1) {
 		//cd "C:\Users\nghia.vt173284\Desktop" 
-		if (userCommand.substr(4, 2) == "c:" || userCommand.substr(4, 2) == "d:") {		
+		if (userCommand.substr(4, 2) == "c:" || userCommand.substr(4, 2) == "d:" || userCommand.substr(4, 2) == "e:") {
 
 			/*NOTICE: This code is not optimize, You should check to determine whether the path is exist or not*/
-
+			
 			string path = userCommand.substr(4, userCommand.size() - 5);
-			Env->currentPath = path;
+			if(checkPath(path))
+				Env->currentPath = path;
+			else 
+				cout << endl << endl << "[ERROR]: " << "This path doesn't exist!!! :((" << endl << endl;
 		}
 
 		//cd "Latex"
 		else {																
 			string folder = userCommand.substr(4, userCommand.size() - 5);
-		
-			/*=====================================================================*/
-			//This is a special case because my shell can't list "Users" in "C:"  and "Desktop" in "C:\Users\nghia.vt173284"
-			bool check = toLower(folder) == "users" || toLower(folder) == "program files" || toLower(folder) == "program files (x86)";
-			if (toLower(Env->currentPath) == "c:" && check) {
-				Env->currentPath += "\\" + folder;
-				return;
-			}
 
-			if (toLower(Env->currentPath) == "c:\\users\\nghia.vt173284" && toLower(folder) == "desktop") {
-				Env->currentPath += "\\" + folder;
-				return;
-			}
-			/*====================================================================*/
-
-			vector<string> ListFolder = dirFolder(Env->currentPath);
-			for (int i = 0; i < ListFolder.size(); ++i) {
-				if (toLower(folder) == toLower(ListFolder[i])) {
-					Env->currentPath += "\\" + ListFolder[i];
-					return;
-				}
-			}
-
-			cout << endl << folder << " is not in this directory!!!" << endl;
+			string path = Env->currentPath + "\\" + folder;
+			if (checkPath(path))
+				Env->currentPath = path;
+			else
+				cout << endl << "[ERROR]: " << folder << "is not in this directory!!!" << endl;
 		}
 	}
 
@@ -70,25 +53,4 @@ void changeCurrentDirection(string userInput, string userCommand, EnvironmentVar
 	else {
 		cout << endl << "Systex Error: " << userInput << " is not recognized as the name of a cmdlet, function, script file, oroperable program.Check the spelling of the name, or if a path was included, verify that the path is correct and try again." << endl;
 	}
-}
-
-vector<string> dirFolder(string path)
-{
-	path += "\\*";
-	WIN32_FIND_DATA FindFileData;
-	HANDLE hFind;
-	vector<string> ListFolder;
-	TCHAR p[200] = {};
-	for (int i = 0; i < path.size(); ++i)
-		p[i] = path[i];
-
-	hFind = FindFirstFile(p, &FindFileData);
-	do
-	{
-		if (FindFileData.dwFileAttributes == 16)
-			ListFolder.push_back(FindFileData.cFileName);
-	} while (FindNextFile(hFind, &FindFileData));
-	FindClose(hFind);
-
-	return ListFolder;
 }
